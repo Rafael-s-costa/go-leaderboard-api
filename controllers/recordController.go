ce57a009d5d2b9836ab2fbe07cd18c38b9ab6df1
Rafael-s-costa <rafael.s.costa96@gmail.com>
2023-01-30 21:12:17 +0000

    Added recordController

--- controllers/recordController.go
+++ controllers/recordController.go
@@ -1,1 +1,14 @@
 package controllers
+
+import (
+	"go-leaderboard-api/pkg/errors"
+	"go-leaderboard-api/pkg/requests"
+	"go-leaderboard-api/pkg/responses"
+)
+
+func GetGameRecords(getGameRecordsRequest *requests.GetGameRecordsRequest) responses.BaseGetRecordsResponse {
+	var response responses.BaseGetRecordsResponse
+	response.ErrorCode = errors.SUCCESS
+
+	return response
+}
